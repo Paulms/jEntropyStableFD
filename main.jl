51c79707bcb29e2b3c7ec314d6d5437a91b2f12c
Paul Mendez <pmendez@ing-mat.udec.cl>
2017-03-13 16:30:53 -0300

    Fix error calculation

--- main.jl
+++ main.jl
@@ -4,6 +4,8 @@
 const μ = 0.01
 const CFL = 0.9
 const Tend = 0.5
+const α = 0.1
+const M = 16000       #Cells in reference solution
 
 #Functions:
 K(u) = μ*u^2
@@ -34,38 +36,57 @@ end
 
 include("numeric_schemes.jl")
 #Save reference data
-#N = 16000
+#N = M
 #dx, dt, ntime, xx, uinit = setup_initial(N)
 #uu3 = Entropy_conservative(uinit,dx,dt,N,ntime)
 #writedlm("burger_1_reference.txt", [xx uu3], '\t')
 
 reference = readdlm("burger_1_reference.txt")
 steps = [200,400,800,1600,3200]
-errors = zeros(2,5)
+errors = zeros(5,5)
 for (i,step) in enumerate(steps)
   println("Testing with ", step, " steps")
   N = step
   dx, dt, ntime, xx, uinit = setup_initial(N)
-  uu = Engquist_Osher(uinit,dx,dt,N,ntime)
-  error = estimate_error(reference, xx, uu, dx, N)
+  uu = Engquist_Osher(uinit,dx,dt,N,ntime)  #MS
+  error = estimate_error(reference[:,2], M, uu, N)
   println("Error: ", error)
   errors[1,i] = error
-  uu2 = Entropy_conservative(uinit,dx,dt,N,ntime)
-  error = estimate_error(reference, xx, uu2, dx, N)
+  uu2 = Entropy_conservative(uinit,dx,dt,N,ntime) #ESC
+  error = estimate_error(reference[:,2], M, uu2, N)
   println("Error: ", error)
   errors[2,i] = error
+  uu3 = Entropy_nonconservative(uinit,dx,dt,N,ntime) #ESNC
+  error = estimate_error(reference[:,2], M, uu3, N)
+  println("Error: ", error)
+  errors[3,i] = error
+  uu4 = Entropy_conservative(uinit,dx,dt,N,ntime, TVD_RK2) #ESC2
+  error = estimate_error(reference[:,2], M, uu4, N)
+  println("Error: ", error)
+  errors[4,i] = error
+  uu5 = Entropy_nonconservative(uinit,dx,dt,N,ntime, TVD_RK2) #ESNC2
+  error = estimate_error(reference[:,2], M, uu5, N)
+  println("Error: ", error)
+  errors[5,i] = error
 end
 
 #Compute order
 order = log2(errors[:,1:4]./errors[:,2:5])
 
+#
+# using DataFrames
+# df = DataFrame(errors);
+# names!(df, map(Symbol,steps));
+# df[:method] = ["MS","ESC","ESNC","ESC2","ESNC2"];
+# println(df)
+
 N=400
 dx, dt, ntime, xx, uinit = setup_initial(N)
 uu = Engquist_Osher(uinit,dx,dt,N,ntime)
-uu2 = Entropy_conservative(uinit,dx,dt,N,ntime)
+uu2 =  Entropy_conservative(uinit,dx,dt,N,ntime, TVD_RK2)
 #Plot
 using(Plots)
-plot(xx, uinit)
-plot!(xx, uu)
-plot!(xx, uu2)
-plot!(reference[:,1], reference[:,2])
+plot(xx, uinit, lab="u0")
+plot!(xx, uu, lab="MS")
+plot!(xx, uu2,lab="ESCN2")
+plot!(reference[:,1], reference[:,2], lab="REF")
--- numeric_schemes.jl
+++ numeric_schemes.jl
@@ -1,3 +1,4 @@
+@enum StepMethod FORWARD_EULER TVD_RK2
 function Engquist_Osher(uinit,dx,dt,N,ntime)
   uu = copy(uinit)
   uleft = uu[1]; uright = uu[N]
@@ -41,7 +42,7 @@ function Engquist_Osher(uinit,dx,dt,N,ntime)
   return uu
 end
 
-function Entropy_conservative(uinit,dx,dt,N,ntime, ϵ = 0.0, Extra_Viscosity = false)
+function Entropy_conservative(uinit,dx,dt,N,ntime, tempSteps = FORWARD_EULER, ϵ = 0.0, Extra_Viscosity = false)
   uu = copy(uinit)
   uleft = uu[1]; uright = uu[N]
   Kleft = K(uleft); Kright = K(uright)
@@ -49,25 +50,19 @@ function Entropy_conservative(uinit,dx,dt,N,ntime, ϵ = 0.0, Extra_Viscosity = f
   percentage = 0
   limit = ntime/5
   println("Starting entropy conservative scheme")
+  utemp = zeros(N)
+  utemp2 = zeros(N)
   for t = 1:ntime
     uold = copy(uu)
-    #Compute diffusion
-    KK = map(K, uold)
-    #update vector
-    j = 1
-    uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uold[j+1])-FluxN(uleft, uold[j])) +
-    dt/dx^2*(KK[j+1] - 2*KK[j] + Kleft) +
-    ϵ*dt/dx^2*(Extra_Viscosity ? uold[j+1]-2*uold[j]+uleft : 0.0)
-
-    for j = 2:(N-1)
-      uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uold[j+1])-FluxN(uold[j-1], uold[j])) +
-      dt/dx^2*(KK[j+1] - 2*KK[j] + KK[j-1])+
-      ϵ*dt/dx^2*(Extra_Viscosity ? uold[j+1]-2*uold[j]+uold[j-1] : 0.0)
+    if (tempSteps == FORWARD_EULER)
+      update_uu_EC(uu, uold, N, dx, dt, uleft, uright, Kleft, Kright, ϵ, Extra_Viscosity)
+    elseif (tempSteps == TVD_RK2)
+      #FIRST Step
+      update_uu_EC(utemp, uold, N, dx, dt, uleft, uright, Kleft, Kright, ϵ, Extra_Viscosity)
+      #Second Step
+      update_uu_EC(utemp2, utemp, N, dx, dt, uleft, uright, Kleft, Kright, ϵ, Extra_Viscosity)
+      uu = 0.5*(uold + utemp2)
     end
-    j = N
-    uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uright)-FluxN(uold[j-1], uold[j])) +
-    dt/dx^2*(Kright - 2*KK[j] + KK[j-1])+
-    ϵ*dt/dx^2*(Extra_Viscosity ? uright-2*uold[j]+uold[j-1]:0.0)
     # Print Progress
     if (t > limit)
       percentage = percentage + 20
@@ -79,12 +74,80 @@ function Entropy_conservative(uinit,dx,dt,N,ntime, ϵ = 0.0, Extra_Viscosity = f
   return uu
 end
 
-function estimate_error(reference, xx, uu, dx, N)
-  dxr = reference[2,1] - reference[1,1]
-  uexact = zeros(N)
-  for i = 1:N
-    j = Int(floor((xx[i] - reference[1,1])/dxr))+1
-    uexact[i] = reference[j,2] + (xx[i] - reference[j,1])*(reference[j+1,2]-reference[j,2])/dxr
+function update_uu_EC(uu, uold, N, dx, dt, uleft, uright, Kleft, Kright, ϵ, Extra_Viscosity)
+  #Compute diffusion
+  KK = map(K, uold)
+  #update vector
+  j = 1
+  uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uold[j+1])-FluxN(uleft, uold[j])) +
+  dt/dx^2*(KK[j+1] - 2*KK[j] + Kleft) +
+  ϵ*dt/dx^2*(Extra_Viscosity ? uold[j+1]-2*uold[j]+uleft : 0.0)
+
+  for j = 2:(N-1)
+    uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uold[j+1])-FluxN(uold[j-1], uold[j])) +
+    dt/dx^2*(KK[j+1] - 2*KK[j] + KK[j-1])+
+    ϵ*dt/dx^2*(Extra_Viscosity ? uold[j+1]-2*uold[j]+uold[j-1] : 0.0)
   end
-  sum(dx*abs(uu - uexact))
+  j = N
+  uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uright)-FluxN(uold[j-1], uold[j])) +
+  dt/dx^2*(Kright - 2*KK[j] + KK[j-1])+
+  ϵ*dt/dx^2*(Extra_Viscosity ? uright-2*uold[j]+uold[j-1]:0.0)
+end
+
+function Entropy_nonconservative(uinit,dx,dt,N,ntime, tempSteps = FORWARD_EULER, ϵ = 0.0, Extra_Viscosity = false)
+  uu = copy(uinit)
+  uleft = uu[1]; uright = uu[N]
+  Kleft = K(uleft); Kright = K(uright)
+  #Print progress
+  percentage = 0
+  limit = ntime/5
+  println("Starting entropy non-conservative scheme")
+  utemp = zeros(N)
+  utemp2 = zeros(N)
+  for t = 1:ntime
+    uold = copy(uu)
+    #update vector
+    if (tempSteps == FORWARD_EULER)
+      update_uu_NC(uu, uold, N, dx, dt, uleft, uright, ϵ, Extra_Viscosity)
+    elseif (tempSteps == TVD_RK2)
+      #FIRST Step
+      update_uu_NC(utemp, uold, N, dx, dt, uleft, uright,ϵ, Extra_Viscosity)
+      #Second Step
+      update_uu_NC(utemp2, utemp, N, dx, dt, uleft, uright, ϵ, Extra_Viscosity)
+      uu = 0.5*(uold + utemp2)
+    end
+    # Print Progress
+    if (t > limit)
+      percentage = percentage + 20
+      limit = limit + ntime/5
+      println(percentage, "% completed")
+    end
+  end
+  println("Completed...")
+  return uu
+end
+
+function update_uu_NC(uu, uold, N, dx, dt, uleft, uright, ϵ, Extra_Viscosity)
+  j = 1
+  uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uold[j+1])-FluxN(uleft, uold[j])) +
+  dt/dx^2*(kvisc(uold[j],uold[j+1])*(uold[j+1]-uold[j]) - kvisc(uleft,uold[j])*(uold[j]-uleft)) +
+  ϵ*dt/dx^2*(Extra_Viscosity ? uold[j+1]-2*uold[j]+uleft : 0.0)
+  for j = 2:(N-1)
+    uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uold[j+1])-FluxN(uold[j-1], uold[j])) +
+    dt/dx^2*(kvisc(uold[j],uold[j+1])*(uold[j+1]-uold[j]) - kvisc(uold[j-1],uold[j])*(uold[j]-uold[j-1]))+
+    ϵ*dt/dx^2*(Extra_Viscosity ? uold[j+1]-2*uold[j]+uold[j-1] : 0.0)
+  end
+  j = N
+  uu[j] = uold[j] - dt/dx * (FluxN(uold[j], uright)-FluxN(uold[j-1], uold[j])) +
+  dt/dx^2*(kvisc(uold[j],uright)*(uright-uold[j]) - kvisc(uold[j-1],uold[j])*(uold[j]-uold[j-1]))+
+  ϵ*dt/dx^2*(Extra_Viscosity ? uright-2*uold[j]+uold[j-1]:0.0)
+end
+
+function estimate_error(reference,M, uu,N)
+  uexact = zeros(N)
+  R = Int(round(M/N))
+  for i = 1:N
+      uexact[i] = 1.0/R*sum(reference[R*(i-1)+1:R*i])
+  end
+  sum(1.0/N*abs(uu - uexact))
 end
