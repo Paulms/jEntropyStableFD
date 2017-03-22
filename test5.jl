90a5c69a8a2b6c70800d0dc3ddfb716fe776d7a4
Paul Mendez <pmendez@ing-mat.udec.cl>
2017-03-22 15:45:45 -0300

    Add test 5

--- test5.jl
+++ test5.jl
@@ -1,44 +1,39 @@
-# Test problem 4
-# Parabolic System
+# Test problem 5
+# Shallow water system with flat bottom
 
 # Parameters:
 const CFL = 0.9
-const Tend = 1.0
+const Tend = 0.2
 #const M = 8000       #Cells in reference solution
-const μ = 0.1
+const μ = 1e-3
+const gr = 9.8    #gravedad
 
 #Functions:
-Flux(u) = u.^2/2
-kk(u) = μ*sum(u.^2)*[1.0 0.0; 0.0 1.0]
-FluxN(ul, ur) = (ur.^2 + ul.*ur + ul.^2)/6.0
+kk(u) = μ*sum(vv(u).^2)*[1.0 0.0; 0.0 1.0]
+vv(u) = [gr*u[1]-0.5*(u[2]/u[1])^2 (u[2]/u[1])]
+FluxN(ul, ur) = [0.25*(ur[1]+ul[1])*(ur[2]/ur[1]+ul[2]/ul[1]); (0.5*gr*0.25*(ur[1]+ul[1])^2+0.25*0.5*(ur[1]+ul[1])*(ur[2]/ur[1]+ul[2]/ul[1])^2)]
 function cdt(u, CFL, dx)
   uu = zeros(size(u,1))
   for j = 1:size(u,1)
     uu[j] = sqrt(sum(kk(u[j,:]).^2))
   end
-  return CFL/(1/dx*maximum(sqrt(u[:,1].^2 + u[:,2].^2))+1/dx^2*2*maximum(uu))
+  h = u[:,1]; q = u[:,2]
+  fu = sqrt(1+(gr*h-q.^2./h.^2).^2+4*q.^2./h.^2)
+  return CFL/(1/dx*maximum(fu)+1/dx^2*2*maximum(uu))
 end
-kvisc(ul,ur) = μ*(sum(ul.^2 + ur.^2))/2.0*[1.0 0.0;0.0 1.0]
+kvisc(ul,ur) = μ*(sum(vv(ul).^2 + vv(ur).^2))/2.0*[1.0 0.0;0.0 1.0]
 
 #Setup initial Conditions
 function setup_initial(N)
-  dx = 5.0/N
-  # (-2.5, 2.5)
-  xx = [i*dx+dx/2-2.5 for i in (0:N-1)]
+  dx = 10.0/N
+  # (-5, 5)
+  xx = [i*dx+dx/2-5.0 for i in (0:N-1)]
   uinit = zeros(N, 2)
   for i = 1:N
-    if -1.5 < xx[i] < -1.3
-      uinit[i,1] = 1.0
-      uinit[i,2] = 0.0
-    elseif -0.5 < xx[i] < 0.5
-      uinit[i,1] = 1.0
-      uinit[i,2] = 1.0
-    elseif 1.3 < xx[i] < 1.5
-      uinit[i,1] = 0.0
-      uinit[i,2] = 1.0
+    if xx[i] < 0.0
+      uinit[i,1] = 2.0
     else
-     uinit[i,1] = 0.0
-     uinit[i,2] = 0.0
+     uinit[i,1] = 1.0
    end
   end
   return dx,xx, uinit
@@ -82,13 +77,13 @@ include("numeric_schemes_nd.jl")
 # println(df)
 # println(dfo)
 
-N=1000
+N=500
 dx, xx, uinit = setup_initial(N)
 @time uu3 = Entropy_nonconservative_nd(uinit,dx,CFL,N,Tend) #ESNC
 
 #Plot
 using(Plots)
-plot(xx, uinit[:,1], lab="u1o",line=(:dot,2))
-plot!(xx, uu3[:,1],lab="ESNC u1")
-plot(xx, uinit[:,2], lab="u2o",line=(:dot,2))
-plot!(xx, uu3[:,2],lab="ESNC u2")
+plot(xx, uinit[:,1], lab="ho",line=(:dot,2))
+plot!(xx, uu3[:,1],lab="ESNC h")
+plot(xx, uinit[:,2]./uinit[:,1], lab="uo",line=(:dot,2))
+plot!(xx, uu3[:,2]./uu3[:,1],lab="ESNC u")
